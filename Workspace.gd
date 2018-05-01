cdb7d93c7a7546fb1ca679a82ff559b0c10d839c
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-05-01 12:50:31 +0300

    Start Button works

--- Workspace.gd
+++ Workspace.gd
@@ -8,7 +8,7 @@ func can_drop_data(position, data):
 	
 func drop_data(drop_position, scene_instance):
 
-	var workspace =get_node("/root/Game/Workspace")
+	var workspace = get_node("/root/Game/Workspace")
 	var game_node = get_node("/root/Game")
 	
 	scene_instance.position = drop_position
